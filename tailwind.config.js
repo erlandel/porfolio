d31d13d89d0f38637312a63cb74683c8527c8e8f
erlandel <delfinoerlan@gmail.com>
2025-01-01 17:55:38 -0500

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
-import { nextui } from "@nextui-org/theme";
+// tailwind.config.js
+const { nextui } = require('@nextui-org/theme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
       },
     },
   },
-  darkMode: "class",
-  darkMode: "class",
+  darkMode: 'class',  // Asegúrate de tener solo una línea de darkMode
   plugins: [nextui()],
 };
